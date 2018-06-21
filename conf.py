ebda50815d108b947e40b0ff1cbb6191cb9edca8
Robin Long <r.long@lancs.ac.uk>
2018-06-21 16:34:12 +0100

    Updated name in conf.py, added gitignore, and added an introduction to the index.

--- conf.py
+++ conf.py
@@ -138,7 +138,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'GridPPDeployment.tex', u'GridPP Deployment Documentation',
+    (master_doc, 'GridPPDeployment.tex', u'GridPP Deployment',
      u'Robin Long', 'manual'),
 ]
 
@@ -148,7 +148,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'gridppdeploymentdocumentation', u'GridPP Deployment Documentation',
+    (master_doc, 'gridppdeployment', u'GridPP Deployment',
      [author], 1)
 ]
 
@@ -159,7 +159,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'GridPPDeployment', u'GridPP Deployment Documentation',
+    (master_doc, 'GridPPDeployment', u'GridPP Deployment',
      author, 'GridPPDeployment', 'One line description of project.',
      'Miscellaneous'),
 ]
--- index.rst
+++ index.rst
@@ -3,8 +3,12 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to GridPP Deployment Documentation's documentation!
+Welcome to the GridPP Deployment Instructions
 ===========================================================
+
+This will hopefully hold the instructions to deploy various GridPP related services. Currently this only includes ARGUS, but others will come soon as we write them at collaboration meetings.
+
+If you spot a mistake, or something that is missing, jump across to github and make an edit!
 
 .. toctree::
    :maxdepth: 2
